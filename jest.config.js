907da21ff54b422e7cfd10ff1629b5dbdea397e7
Claudio Olivares <ca.olivares91@gmail.com>
2026-08-25 16:09:25 -0400

    feat: se agreg regla covertura codigo

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,21 @@ export default {
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   clearMocks: true,
+
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/generated/prisma/**',
+    '!src/**/*.test.ts',
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
